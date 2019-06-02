63d6b7ad0a15da5b4d5f168b23ae8f7025ad6112
wandl <hnxyhcwdl1003@163.com>
2019-06-02 23:35:45 +0800

    修改

---
+++ src/main/java/ro/fortsoft/pf4j/spring/boot/ext/update/MavenUpdateRepository.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2018, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package ro.fortsoft.pf4j.spring.boot.ext.update;
+
+import java.net.URL;
+import java.util.Map;
+
+import org.springframework.cloud.deployer.resource.maven.MavenProperties;
+
+import ro.fortsoft.pf4j.update.FileDownloader;
+import ro.fortsoft.pf4j.update.PluginInfo;
+import ro.fortsoft.pf4j.update.UpdateRepository;
+
+/**
+ * TODO
+ * @author 		： <a href="https://github.com/vindell">vindell</a>
+ */
+public class MavenUpdateRepository implements UpdateRepository {
+	
+    private static final String MAVEN_REPOSITORY = "maven";
+    
+    private URL url;
+    private Map<String, PluginInfo> plugins;
+    private MavenProperties mavenProperties;
+	private PluginInfoProvider pluginInfoProvider;
+	
+	public MavenUpdateRepository(MavenProperties mavenProperties,PluginInfoProvider pluginInfoProvider) {
+		this.mavenProperties = mavenProperties;
+		this.pluginInfoProvider = pluginInfoProvider;
+	}
+	
+	@Override
+    public String getId() {
+        return MAVEN_REPOSITORY;
+    }
+
+	@Override
+	public URL getUrl() {
+		return url;
+	}
+	
+	@Override
+    public Map<String, PluginInfo> getPlugins() {
+        if (plugins == null) {
+        	plugins = getPluginInfoProvider().plugins();
+        	if(plugins != null) {
+        		plugins.entrySet().forEach(info -> {
+            		info.getValue().setRepositoryId(MAVEN_REPOSITORY);
+            	});
+        	}
+        }
+        return plugins;
+    }
+	
+    @Override
+    public PluginInfo getPlugin(String coordinates) {
+        return getPlugins().get(coordinates);
+    }
+
+    /**
+     * Causes plugins.json to be read again to look for new updates from repos
+     */
+    @Override
+    public void refresh() {
+        plugins = null;
+    }
+
+    @Override
+    public FileDownloader getFileDownloader() {
+        return new MavenFileDownloader(mavenProperties);
+    }
+
+	public PluginInfoProvider getPluginInfoProvider() {
+		return pluginInfoProvider;
+	}
+
+}
